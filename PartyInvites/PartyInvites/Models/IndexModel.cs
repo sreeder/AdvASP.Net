8e99a047c475515b980099788dc8a7c0cf6ccb41
Stephanie Reeder <stephanie.lyn.gmail.com>
2017-01-18 18:18:36 -0700

    add indexmodel class stuff pt 2)

---
+++ PartyInvites/PartyInvites/Models/IndexModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PartyInvites.Models
+{
+    public class IndexModel
+    {
+        public string Greeting { get; set; }
+        public DateTime CurrentTime { get; set; }
+
+        public int TimeUntilTheParty
+        {
+            get
+            {
+                var timespan = (new DateTime(2017, 1, 21, 20, 0, 0) - CurrentTime);
+                return (int)Math.Round(timespan.TotalHours);
+            }
+        }
+    }
+}
